f939b832ce62b48ccac56d0878497e976f79f307
sascha-lr <174216621+sascha-lr@users.noreply.github.com>
2026-05-04 16:21:06 +0200

    Add DOM IIFE object, basic styling

--- app.js
+++ app.js
@@ -2,13 +2,10 @@
 
 const game = (() => {
     const board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
     ];
-
-    let currentPlayer;
-    let turns = 0;
 
     const players = [
         {
@@ -21,23 +18,26 @@ const game = (() => {
         },
     ];
 
+    let turns = 0;
+    let currentPlayer = players[1];
+
     const setSymbol = (symbol) => {
         players[0].currentSymbol = symbol;
-        players[1].currentSymbol = symbol === 1 ? 2 : 1;
+        players[1].currentSymbol = symbol === 'X' ? 'O' : 'X';
     }
 
     const changeCurrentPlayer = () => {
         currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     }
 
-    const takeInput = (row, column) => {
+    const setInput = (row, column) => {
         board[row][column] = currentPlayer.currentSymbol;
     }
 
     const resetGame = () => {
         for (let row of board) {
             for (let i = 0; i < row.length; i++) {
-                row[i] = 0;
+                row[i] = '';
             }
         }
         for (let player of players) {
@@ -56,12 +56,11 @@ const game = (() => {
 
     const announceWinner = () => {
         currentPlayer.score++;
-        console.log(`Congratulations! Player ${players.indexOf(currentPlayer) + 1}, won the game!`);
     }
 
     const checkIfWon = () => {
         for (let row of board) {
-            if (!row.find((element) => element === 0)) {
+            if (!row.find((element) => element === '')) {
                 if (row[0] === row[1] && row[1] === row[2]) {
                     return true;
                 }
@@ -84,33 +83,78 @@ const game = (() => {
         }
     }
 
-    const displayBoard = () => {
-        for (let row of board) {
-            console.log(row);
-        }
-    }
+    const getBoard = () => board;
 
-    const takeTurn = (row, column) => {
-        console.log('------------');
+    const takeTurn = (element) => {
+        const cell = DOM.getInput(element);
         turns++;
         changeCurrentPlayer();
-        console.log(`Player ${players.indexOf(currentPlayer) + 1}, please where do you want to put your ${currentPlayer.currentSymbol}?`);
-        takeInput(row, column);
-        displayBoard();
+        setInput(cell.dataset.row, cell.dataset.column);
         if (turns > 4 && checkIfWon()) {
             announceWinner();
-            resetGame();
         }
     }
 
-    const start = () => {
-        console.log('Game is starting! Player 1, please pick a symbol');
-        setSymbol(1);
-        displayBoard();
+    const start = (symbol) => {
+        setSymbol(symbol);
     }
 
-    return { start, takeTurn };
+    return { start, getBoard, takeTurn };
 
 })();
 
 
+const DOM = (() => {
+
+    // Elements
+    const gameBoard = document.querySelector('.game-board');
+    const form = document.querySelector('form');
+
+    //Functions
+    const displayBoard = () => {
+        const container = document.createElement('div');
+        container.classList.add('container');
+
+        const boardArray = game.getBoard();
+        let rowIndex = 0;
+        for (let row of boardArray) {
+            for (let i = 0; i < row.length; i++) {
+                const cell = document.createElement('button');
+                cell.dataset.row = `${rowIndex}`;
+                cell.dataset.column = `${i}`;
+                cell.classList.add('cell');
+                cell.innerText = `${row[i]}`;
+                container.appendChild(cell);
+            }
+            rowIndex++;
+        }
+        gameBoard.appendChild(container);
+    }
+
+    const removeBoard = () => {
+        gameBoard.innerHTML = '';
+    }
+
+    const getInput = (element) => element;
+
+    // Event Listeners
+    gameBoard.addEventListener('click', (e) => {
+        if (e.target.closest('.cell')) {
+            const cell = e.target.closest('.cell');
+            const board = game.getBoard();
+            if (board[cell.dataset.row][cell.dataset.column]) return;
+            game.takeTurn(cell);
+            removeBoard();
+            displayBoard();
+        }
+    })
+
+    form.addEventListener('submit', () => {
+        const formData = new FormData(form);
+        game.start(formData.get('symbol'));
+        displayBoard();
+    })
+
+    return { getInput };
+
+})();
